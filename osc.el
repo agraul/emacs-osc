a773409c07c9a61d5c59287e22c7f2576ec6e8cf
Alexander Graul <agraul@suse.com>
2021-04-16 16:47:26 +0200

    osc-run-status opens a buffer with the result

--- osc.el
+++ osc.el
@@ -3,39 +3,45 @@
 ;;; Magit-inspired osc procelain
 ;;; Code:
 
-(defun osc-run (subcmd &optional directory &rest args)
+(defvar osc--status-buffer-name "*osc-status*")
+
+(defun osc-run (subcmd buf &optional directory &rest args)
   "Run an osc command synchronously, specified as SUBCMD.
 
+BUF is the buffer that the output is written to.
 DIRECTORY  can be used to change the working directory for the call.
 Any ARGS given will be appended to the command."
   (let ((default-directory (or directory
                                default-directory)))
     (if args
-        (call-process "osc" nil "*osc-out*" nil subcmd (format "%s" args))
-      (call-process "osc" nil "*osc-out*" nil subcmd))))
+        (call-process "osc" nil buf nil subcmd (format "%s" args))
+      (call-process "osc" nil buf nil subcmd))))
 
 (defun osc-run-status ()
   "Run osc status in the current package directory.
 
 Also works if the current working directory is a subdirectory of a package directory."
+  (interactive)
   (if-let ((osc-dir (osc--find-osc-working-directory default-directory)))
-    (when (osc--package-directory-p osc-dir)
-      (osc-run "status" osc-dir))
+      (when (osc--package-directory-p osc-dir)
+        (kill-buffer osc--status-buffer-name)
+        (osc-run "status" osc--status-buffer-name osc-dir)
+        (switch-to-buffer osc--status-buffer-name))
     (message "Not in an osc package directory.")))
 
 (defun osc--working-directory-p (dir)
-  "Return wether the passed DIR is an osc working directory or not."
+  "Return whether the passed DIR is an osc working directory or not."
   (and (file-exists-p (expand-file-name ".osc" dir))
        dir))
 
 (defun osc--package-directory-p (osc-dir)
-  "Return wether the passed OSC-DIR is an osc package working directory or not.
+  "Return whether the passed OSC-DIR is an osc package working directory or not.
 
 \(osc--working-directory-p osc-dir) must be true."
   (file-exists-p (expand-file-name "_package" (expand-file-name ".osc" osc-dir))))
 
 (defun osc--project-directory-p (osc-dir)
-  "Return wether the passed OSC-DIR is an osc project working directory or not.
+  "Return whether the passed OSC-DIR is an osc project working directory or not.
 
 \(osc--working-directory-p osc-dir) must be true."
   (not (osc--package-directory-p osc-dir)))
